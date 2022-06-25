b5a37c2e6ccf3f8c17c28fb464118b7306a52e8f
warisjamal <warisjamal92@gmail.com>
2022-06-25 18:45:52 +0530

    Done

--- cypress/e2e/cognito Login/cognitoLogin.cy.js
+++ cypress/e2e/cognito Login/cognitoLogin.cy.js
@@ -3,11 +3,7 @@
 describe('congnito login test',()=>{
 
     it('Testing Starts',()=>{
-        cy.visit('https://kna.qa.learnwithatom.com/');
-        // cy.wait(10000);
-        cy.get('.visible-lg #signInFormUsername').type('ktp.author');
-        cy.get('.visible-lg #signInFormPassword').type('Kaplan123@');
-        cy.get('.visible-lg .submitButton-customizable').click();
+     
     })
 
 })
