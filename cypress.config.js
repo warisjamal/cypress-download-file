0a6ce070a20a3d97cdec4cf12fcb5a0c12646f7a
warisjamal <warisjamal92@gmail.com>
2022-06-25 12:08:11 +0530

    update-config

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,14 @@
 const { defineConfig } = require("cypress");
 const {downloadFile} = require('cypress-downloadfile/lib/addPlugin');
-const { isFileExist } = require('cy-verify-downloads');
+
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  downloadsFolder: 'cypress/downloads',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {downloadFile}),
-      on('task', { isFileExist })
     },
     experimentalSessionAndOrigin: true,
-    requestTimeout: 5000,
-    "defaultCommandTimeout": 100000,
-    "pageLoadTimeout": 100000
   },
 });
